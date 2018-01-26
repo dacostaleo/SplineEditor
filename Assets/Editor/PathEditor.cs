40366f1c403e9378031c80d6a29d542aa57ec374
dacostaleo <dacostaleo@gmail.com>
2018-01-26 17:08:18 +0100

    Proy

---
+++ Assets/Editor/PathEditor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(PathCreator))]
+public class PathEditor : Editor {
+
+    PathCreator creator;
+    Path path;
+
+    private void OnSceneGUI()
+    {
+        Input();
+        Draw();
+    }
+
+    void Input()
+    {
+        Event guiEvent = Event.current;
+        Vector2 mousePos = HandleUtility.GUIPointToWorldRay(guiEvent.mousePosition).origin;
+
+        if (guiEvent.type == EventType.mouseDown && guiEvent.button == 0 && guiEvent.shift)
+        {
+            Undo.RecordObject(creator, "Ad Segment");
+            path.AddSegment(mousePos);
+        }
+    }
+
+    void Draw()
+    {
+        for (int i = 0; i < path.numSegments; i++)
+        {
+            Vector2[] points = path.GetPointsInSegment(i);
+            Handles.color = Color.black;
+            Handles.DrawLine(points[1], points[0]);
+            Handles.DrawLine(points[2], points[3]);
+
+
+            Handles.DrawBezier(points[0], points[3], points[1], points[2], Color.green, null, 2);
+
+        }
+
+        Handles.color = Color.red;
+        for (int i = 0; i < path.numPoints; i++)
+        {
+            Vector2 newPos = Handles.FreeMoveHandle(path[i], Quaternion.identity, .1f, Vector2.zero, Handles.CylinderHandleCap);
+            if (path[i] != newPos)
+            {
+                Undo.RecordObject(creator, "MovePoint");
+                path.MovePoint(i, newPos);
+            }
+        }
+    }
+
+    void OnEnable()
+    {
+        creator = (PathCreator)target;
+        if (creator.path == null)
+        {
+            creator.CreatePath();
+        }
+        path = creator.path;
+    }
+
+}
---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,98 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Path
+{
+
+    [SerializeField, HideInInspector]
+    List<Vector2> points;
+
+    public Path(Vector2 center)
+    {
+        points = new List<Vector2>()
+        {
+            center + Vector2.left,
+            center + (Vector2.left + Vector2.up) * 0.5f,
+            center + (Vector2.right + Vector2.down) * 0.5f,
+            center + Vector2.right
+        };
+        
+    }
+
+    public Vector2 this[int i]
+    {
+        get
+        {
+            return points[i];
+        }
+    }
+
+    public int numSegments
+    {
+        get
+        {
+            return (points.Count - 1) / 3;
+        }
+    }
+
+    public int numPoints
+    {
+        get
+        {
+            return points.Count;
+        }
+    }
+
+    public void AddSegment(Vector2 anchorPos)
+    {
+        points.Add(2 * points[points.Count - 1] - points[points.Count - 2]);
+        points.Add((anchorPos + points[points.Count - 1]) * 0.5f);
+        points.Add(anchorPos);
+    }
+
+    public Vector2[] GetPointsInSegment(int i)
+    {
+        return new Vector2[]
+        {
+            points[i*3],
+            points[i*3 + 1],
+            points[i * 3 + 2],
+            points[i*3+3]
+        };
+    }
+
+    public void MovePoint(int i, Vector2 pos)
+    {
+        Vector2 deltaMove = pos - points[i];
+        points[i] = pos;
+
+        if (i % 3 == 0)
+        {
+            if (i + 1 < points.Count)
+            {
+                points[i + 1] += deltaMove;
+            }
+            if (i - 1 >= 0)
+            {
+                points[i - 1] += deltaMove;
+            }
+        }
+        else
+        {
+            bool nextPointIsAnchor = (i + 1) % 3 == 0;
+            int correspondingControlIndex = nextPointIsAnchor ? i + 2 : i - 2;
+            int anchorIndx = nextPointIsAnchor ? i + 1 : i - 1;
+
+            if (correspondingControlIndex >= 0 && correspondingControlIndex < points.Count)
+            {
+                float dst = (points[anchorIndx] - points[correspondingControlIndex]).magnitude;
+                Vector2 dir = (points[anchorIndx] - points[i]).normalized;
+                points[correspondingControlIndex] = points[anchorIndx] + dir * dst;
+            }
+        }
+
+    }
+
+}
